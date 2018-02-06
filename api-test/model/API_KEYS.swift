e4566018da5a691768629cba16fb883f34f68ae0
Salman Fakhri <salmanfakhri2@gmail.com>
2018-02-06 12:08:39 -0500

    testing constants from work

---
+++ api-test/model/API_KEYS.swift
@@ -0,0 +1,16 @@
+//
+//  API_KEYS.swift
+//  api-test
+//
+//  Created by Salman Fakhri on 2/2/18.
+//  Copyright © 2018 Salman Fakhri. All rights reserved.
+//
+
+import Foundation
+
+
+struct API_KEY {
+    
+    static let keyInstance = API_KEY();
+    let key = "6f64ce3504d7fec323575df1328e3685"
+}
